77c114f595b35d801722b5e4fe0ab41f1b49c156
Firman47 <122626748+Firman47@users.noreply.github.com>
2024-10-22 11:35:02 +0700

    ADD CHAT

--- src/App.vue
+++ src/App.vue
@@ -55,4 +55,19 @@ watch(theme, (newValue) => {
 onMounted(() => {
   document.documentElement.setAttribute("data-theme", theme.value);
 });
+
+onMounted(() => {
+  var Tawk_API: any = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function () {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/671724e32480f5b4f59112eb/1iap6g6qj";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+});
 </script>
+<!--Start of Tawk.to Script-->
